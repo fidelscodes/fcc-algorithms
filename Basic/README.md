641b5ee2bb3b0413cce66c37b1deeeab522fc72a
fidelscodes <fidelscodes@gmail.com>
2015-09-14 18:02:12 -0400

    first commit

---
+++ Basic/README.md
@@ -0,0 +1,9 @@
+# FreeCodeCamp Bonfires
+
+These are my solutions to the FreeCodeCamp Basic Algorithm Scripting challenges.
+
+These might not be the most elegant or concise solutions but these are my solutions as a newbie to programming and JavaScript.
+
+Ideally, this will serve as a way to look back at how far I've come.
+
+Any feedback is most welcome!
---
+++ Basic/reverse-a-string.js
@@ -0,0 +1,16 @@
+/*
+http://freecodecamp.com/challenges/bonfire-reverse-a-string
+INSTRUCTIONS:
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+*/
+
+function reverseString(str) {
+  str = str.split('').reverse().join('');
+  return str;
+}
+
+reverseString('hello');
