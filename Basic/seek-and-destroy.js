cca963a15843873234ac14ccf67531bd1ae8a43f
Fidel <fidelscodes@gmail.com>
2016-01-21 09:27:01 +0000

    Updated solution to Seek and Destroy

--- Basic/seek-and-destroy.js
+++ Basic/seek-and-destroy.js
@@ -6,6 +6,22 @@ Remove all elements from the initial array that are of the same value as these
 arguments.
 */
 
+/* 
+My original code before the bonfire was updated to include a test with 3 args
+
+function destroyer(arr) {
+    var arg1 = arguments[1];
+    var arg2 = arguments[2];
+    var filtered = arr.filter(function (val, index, array) {
+        return val !== arg1 && val !== arg2;
+    });
+    return filtered;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+*/
+
 function destroyer(arr) {
   // converts the arguments into an actual array
   var args = Array.prototype.slice.call(arguments);
